2e685c21cf1925ad738f7851ee580766da2c437e
Danyyl <danyyl.con@gmail.com>
2021-09-11 18:39:32 -0400

    create app/add few basic components

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({title}) => {
+    const onClick = () => {
+        const newDiv = document.createElement('div');
+        newDiv.appendChild(document.createTextNode('New div made by button click'));
+        document.body.appendChild(newDiv);
+    }
+    
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button bgColor='green' text='Add' onClick={onClick}/>
+        </header>
+    )
+};
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+};
+
+Header.propTypes = {
+    title: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func
+};
+
+
+export default Header;
